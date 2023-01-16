531da20985f22ae9cdb0c71113a116d451d937d6
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-01-16 09:49:28 +0800

    fix: submit button refresh page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React,{ Component } from "react";
 
 class App extends Component {
   constructor(props) {
-    
+    super(props);
+
+    this.onClickBtn = this.onClickBtn.bind(this);
   }
+
+  onClickBtn() {
+    console.log("Submit button has been clicked!");
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +18,7 @@ class App extends Component {
           <label for="task">Add your task here:</label>
           <br />
           <input name="task"></input>
-          <button type="submit" data-submit-btn>Submit</button>
+          <button type="button" data-submit-btn onClick={this.onClickBtn}>Submit</button>
         </form>
       </div>
     );
