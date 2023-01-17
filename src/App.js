2c7a8c315d61d566a5aca2ef525f9452b553206d
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-01-17 10:36:35 +0800

    update: push input textbox to state.taskArray

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ class App extends Component {
     super(props);
 
     this.onClickBtn = this.onClickBtn.bind(this);
+
+    this.state = {
+      taskArray: [],
+    }
   }
 
   onClickBtn() {
     console.log("Submit button has been clicked!");
+
+    // Read current task array from the state.
+    const currentTaskArray = this.state.taskArray;
+    const inputTextboxValue = document.querySelector("[data-input-textbox]").value;
+    // Push the current input textbox value into the current task array.
+    currentTaskArray.push(inputTextboxValue);
+    // Assign the state to current task array.
+    this.setState({
+      taskArray: currentTaskArray,
+    })
+    console.log(`Task aray ${this.state.taskArray}`);
   }
 
   render() {
@@ -17,7 +32,7 @@ class App extends Component {
         <form>
           <label for="task">Add your task here:</label>
           <br />
-          <input name="task"></input>
+          <input name="task" data-input-textbox></input>
           <button type="button" data-submit-btn onClick={this.onClickBtn}>Submit</button>
         </form>
       </div>
